cfe7a1426fe404d12270dda42a0b5e5e87333d16
Jan Müller <jan.mueller@nexiles.com>
2015-09-28 16:36:51 +0200

    [FIX] project_timesheet: allow date strings without time fixes an issue that occured when changing the project of a task this modification is already included in branch 8.0-merge-upstream

--- addons/project_timesheet/project_timesheet.py
+++ addons/project_timesheet/project_timesheet.py
@@ -118,9 +118,12 @@ class project_work(osv.osv):
         vals_line['user_id'] = vals['user_id']
         vals_line['product_id'] = result['product_id']
         if vals.get('date'):
-            timestamp = datetime.datetime.strptime(vals['date'], tools.DEFAULT_SERVER_DATETIME_FORMAT)
-            ts = fields.datetime.context_timestamp(cr, uid, timestamp, context)
-            vals_line['date'] = ts.strftime(tools.DEFAULT_SERVER_DATE_FORMAT)
+            if len(vals['date']) > 10:
+                timestamp = datetime.datetime.strptime(vals['date'], tools.DEFAULT_SERVER_DATETIME_FORMAT)
+                ts = fields.datetime.context_timestamp(cr, uid, timestamp, context)
+                vals_line['date'] = ts.strftime(tools.DEFAULT_SERVER_DATE_FORMAT)
+            else:
+                vals_line['date'] = vals['date']
 
         # Calculate quantity based on employee's product's uom
         vals_line['unit_amount'] = vals['hours']
